3e761affd9230040a6157d8f0a0ce4f248c84806
Jeferson Luis <jgjefersonluis@gmail.com>
2022-08-11 13:42:38 -0300

    senna for

---
+++ PraticaDesvendandoJava/src/com/java/senna/Senna.java
@@ -0,0 +1,10 @@
+package com.java.senna;
+
+public class Senna {
+
+	public static void main(String[] args) {
+		System.out.println("1, 2, 3, 4, 5, 6 ... 60");
+
+	}
+
+}
---
+++ PraticaDesvendandoJava/src/com/java/senna/SennaFor.java
@@ -0,0 +1,15 @@
+package com.java.senna;
+
+public class SennaFor {
+
+	public static void main(String[] args) {
+		System.out.println("Números da Senna! ");
+		
+		for (int i = -1; i <= 60 ; i ++){
+			System.out.println(i + ",");
+		}
+		System.out.println();
+
+	}
+
+}
