86887a4aa884b3dc7f0876affbd784be9968fb30
Jeferson Luis <jgjefersonluis@gmail.com>
2022-08-11 13:48:37 -0300

    senna do while

---
+++ PraticaDesvendandoJava/src/com/java/senna/SennaDoWhile.java
@@ -0,0 +1,16 @@
+package com.java.senna;
+
+public class SennaDoWhile {
+
+	public static void main(String[] args) {
+		System.out.println("Números da Senna!");
+		int i = 1;
+		do {
+			System.out.println(i + ",");
+			i++;
+		} while (i <= 60);
+		System.out.println();
+
+	}
+
+}
