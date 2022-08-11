36166dac58df2217da2ce8e81e5f62736b3b71d1
Jeferson Luis <jgjefersonluis@gmail.com>
2022-08-11 13:55:24 -0300

    tabuada

---
+++ PraticaDesvendandoJava/src/com/java/tabuada/Tabuada.java
@@ -0,0 +1,15 @@
+package com.java.tabuada;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		System.out.println("Tabuada do: " + numero);
+		for (int i = 1; 1 <= 10; i++) {
+			int resultado = numero * i;
+			System.out.println(numero + " * " + i + " = " + resultado);
+		}
+
+	}
+
+}
